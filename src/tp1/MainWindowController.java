54bbaccc28b76539f0c6d8075fc7b78ae26a4817
Sebastian-andreas SIMON <Sebastian-andreas.SIMON@crie.i-univ-tlse2.fr>
2018-10-11 17:51:10 +0200

    FXML first draft

--- src/tp1/MainWindowController.java
+++ src/tp1/MainWindowController.java
@@ -7,7 +7,13 @@ package tp1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 
 /**
  * FXML Controller class
@@ -16,6 +22,18 @@ import javafx.fxml.Initializable;
  */
 public class MainWindowController implements Initializable {
     ContexteIdentification contexte;
+    @FXML
+    private ComboBox<?> cmbb_haircolor;
+    @FXML
+    private ComboBox<?> cmbb_eyeColor;
+    @FXML
+    private Slider sl_hairLength;
+    @FXML
+    private ListView<?> lsb_noseShape;
+    @FXML
+    private Button btn_reset;
+    @FXML
+    private Button btn_save;
     /**
      * Initializes the controller class.
      */
@@ -25,6 +43,14 @@ public class MainWindowController implements Initializable {
     }    
     public void setContexte(ContexteIdentification contexte){
         this.contexte = contexte;        
+    }
+
+    @FXML
+    private void reset(ActionEvent event) {
+    }
+
+    @FXML
+    private void save(ActionEvent event) {
     }
     
 }
